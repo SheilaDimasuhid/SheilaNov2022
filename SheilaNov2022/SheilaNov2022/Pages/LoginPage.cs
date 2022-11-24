d616f3d1e1b9d1702e2ce4aaf342233298819373
SheilaDimasuhid <dimasuhidsheila@outlook.com>
2022-11-24 20:03:00 +1300

    POM implemented

---
+++ SheilaNov2022/SheilaNov2022/Pages/LoginPage.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheilaNov2022.Pages
+{
+    public class LoginPage
+    {
+        public void LoginActions(IWebDriver driver)
+        {
+            
+            //maximize the browser
+            driver.Manage().Window.Maximize();
+
+            // launch turnup portal
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+            Thread.Sleep(1000);
+
+            // identify username textbox and enter valid username
+            IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+            usernameTextbox.SendKeys("hari");
+
+            // identify password textbox and enter valid password
+            IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+            passwordTextbox.SendKeys("123123");
+
+            // click login button
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+            loginButton.Click();
+
+
+            /*
+             * 
+            // check if user has logged in successfully 
+            IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+            if (helloHari.Text == "Hello hari!")
+            {
+                Console.WriteLine("Logged in successfully, test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Login failed, test failed.");
+            }
+
+            */
+        }
+    }
+}
